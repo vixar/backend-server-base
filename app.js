4604532c22ff460fc8bfd1a88944d02dc8dee174
vickry Miguel Sanatana Rincon <vickry.santana@gmail.com>
2018-03-27 07:25:32 -0400

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 
@@ -13,28 +14,38 @@ var app = express();
 
 
 
+// body-parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+
+
+
 // Conexion a ala base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/operacionesDB', (err, res) => {
 
     if (err) throw err;
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 
-})
+});
 
 
+
+// Importar rutas
+
+var appRoutes = require('./Routes/app');
+var userRoutes = require('./Routes/user');
+var loginRoutes = require('./Routes/login');
 
 
 
 
 // Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-
-})
+app.use('/user', userRoutes);
+app.use('/login', loginRoutes)
+app.use('/', appRoutes);
 
 
 
@@ -42,4 +53,4 @@ app.get('/', (req, res, next) => {
 // escuchar peticiones
 app.listen(3000, () => {
     console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
-})
+});
